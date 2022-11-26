58427861523fdad4c1246b47c6c240ccbcc88ece
koheis <koheis@koheis-VirtualBox>
2022-11-25 22:07:13 -0800

    Insert success.

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>User Login Form</title>
+</head>
+<body style="background-color: bisque">
+<form method="post" action="userLogin.php">
+<fieldset> <legend><h3> Login Form </h3></legend>
+<p><strong>username:</strong><br/>
+<input type="text" name="username"/></p>
+<p><strong>password:</strong><br/>
+<input type="password" name="password"/></p>
+<p><input type="submit" name="submit" value="login"/></p>
+</fieldset>
+</form>
+    <br>
+    <a href ="createAccount.php">Create Your Account</a>
+</body>
+</html>
