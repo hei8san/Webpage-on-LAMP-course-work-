75bf9243dea13587e36eb44a918868ec3ff77f21
koheis <koheis@koheis-VirtualBox>
2022-11-26 14:46:24 -0800

    added redirecting feature in logout with jquery

--- logout.php
+++ logout.php
@@ -5,9 +5,29 @@ session_destroy();
 <html>
     <head>
         <title>Log out</title>
+        <script 
+            src="https://code.jquery.com/jquery-git.js">
+        </script>  
     </head>
     <body>
-        <p>Log out is success</p>
+        <h2>Log out success</h2>
+
+        <div class="redirect">
+            <button>Redirect</button>
+        </div>
+        <script>
+            // click event on button
+            $("button").click(function () {
+                $(".redirect").text("Redirecting....")
+
+                // storing url and time
+                let delay = 5000;
+                let url = "https://ksawabevm.cosc.okc/cs213project/homePage.php";
+                setTimeout(function () {
+                    location = url;
+                }, 5000)
+            })
+        </script>
     </body>
 </html>
 
