9f953676fa643c6d3d0b60aea13ca8b4b3ecd1b0
koheis <koheis@koheis-VirtualBox>
2022-11-26 21:46:14 -0800

    query for display all user's information

--- accountPage.php
+++ accountPage.php
@@ -2,7 +2,7 @@
 session_start();
 echo $_SESSION['username'];
 ?>
-<html>
+<!--<html>
     <head>
         <title>Account Page</title>
     </head>
@@ -12,6 +12,38 @@ echo $_SESSION['username'];
         <h2>This is account page</h2>
         
     </body>
+</html>-->
+<html>
+<head>
+    <script type='text/javascript'>
+        function addFields(){
+            // Generate a dynamic number of inputs
+            var number = document.getElementById("member").value;
+            // Get the element where the inputs will be added to
+            var container = document.getElementById("container");
+            // Remove every children it had before
+            while (container.hasChildNodes()) {
+                container.removeChild(container.lastChild);
+            }
+            for (i=0;i<number;i++){
+                // Append a node with a random text
+                container.appendChild(document.createTextNode("Member " + (i+1)));
+                // Create an <input> element, set its type and name attributes
+                var input = document.createElement("input");
+                input.type = "text";
+                input.name = "member" + i;
+                container.appendChild(input);
+                // Append a line break 
+                container.appendChild(document.createElement("br"));
+            }
+        }
+    </script>
+</head>
+<body>
+    <input type="text" id="member" name="member" value="">Number of members: (max. 5)<br />
+    <a href="#" id="filldetails" onclick="addFields()">Fill Details</a>
+    <div id="container"/>
+</body>
 </html>
 
 
