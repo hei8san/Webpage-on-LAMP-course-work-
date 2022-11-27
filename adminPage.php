73e92a521d44f4af891ccae786eb27df0b8916e6
koheis <koheis@koheis-VirtualBox>
2022-11-26 18:20:31 -0800

    query for display all user's information

--- adminPage.php
+++ adminPage.php
@@ -1,38 +1,89 @@
 <?php
 $displayBlock_adminPage;
+$displayTable;
 
-if ((filter_input(INPUT_POST, 'username')) || (filter_input(INPUT_POST, 'password'))) {
-    header("Location: login.php");
-    exit;
-}
-
-//connect to server and select database
-$mysqli = mysqli_connect("localhost", "cs213user", "letmein", "finalProjectDB");
-
-//create and issue the query
-$targetname = filter_input(INPUT_POST, 'username');
-$targetpasswd = filter_input(INPUT_POST, 'password');
-$sqlLogin = "SELECT * FROM userLogin WHERE username = '" . $targetname .
-        "' AND password = SHA1('" . $targetpasswd . "')";
-
-$resultLogin = mysqli_query($mysqli, $sqlLogin) or die(mysqli_error($mysqli));
-if ($resultLogin == 1) {
-//    $displayBlock_AccountPage = "<p>Success</p>";
-    $_SESSION["username"] = $targetname;
-    header("Location: accountPage.php"); 
-} else {
-//    $displayBlock_AccountPage = "<p>Unsuccess</p>";
-    header("Location: login.php"); 
+if (isset($_POST['conditions'])) {
+    $condition = $_POST['conditions'];
+    if ($condition == 'all') {
+        //connect to server and select database
+        $mysqli = mysqli_connect("localhost", "cs213user", "letmein", "finalProjectDB");
+        $sql_AllDisplay = "SELECT username, CONCAT(CONCAT(firstname, ' '), lastname) Fullname, email, age FROM userInfo";
+        
+        $result_AllDisplay = mysqli_query($mysqli, $sql_AllDisplay) or die(mysqli_error($mysqli));
+        if (mysqli_num_rows($result_AllDisplay) > 0) {
+            $displayTable = "<table> <tr><th>username</th><th>Full name</th><th>Email</th><th>Age</th></tr>";
+            while ($newArray = mysqli_fetch_array($result_AllDisplay, MYSQLI_ASSOC)) {
+                $username = $newArray['username'];
+                $fullname = $newArray['Fullname'];
+                $email = $newArray['email'];
+                $age = $newArray['age'];
+                $displayTable .= "<tr> <td> " . $username . "</td> <td>" . $fullname . "</td> <td>" . $email . "</td><td>" . $age . "</td></tr>";
+            }
+            $displayTable .= "</table>";
+        } else {
+            
+        }
+    } else if ($condition == 'age') {
+        
+    } else if ($condition == 'initial') {
+        
+    } else if ($condition == 'email') {
+        
+}}
+ else {
+    $displayBlock_adminPage = "<h2>Select Options</h2>
+        <form action='' method = 'post'>
+            <label for='conditions'>Choose conditions:</label>
+            <select name='conditions' id='conditions'>
+                <option value='all'>All users</option>
+                <option value='age'>Age</option>
+                <option value='initial'>Initial letter</option>
+                <option value='email'>Email</option>
+            </select>
+            <br><br>
+            <input type='submit' value='Submit'>
+        </form>";
 }
 ?>
 <html>
     <head>
         <title>Admin</title>
+         <style>
+            th {
+
+                border-right: 1px solid #000000;
+                border-bottom: 1px solid #000000;
+                border-top: 1px solid #000000;
+                border-left: 1px solid #000000;
+                color: blue;
+                font-weight: bold;
+            }
+            td {
+                padding: 5px;
+                border-right: 1px solid #000000;
+                border-bottom: 1px solid #000000;
+                border-top: 1px solid #000000;
+                border-left: 1px solid #000000;
+                text-align: left;
+                
+                color: blue;
+            }
+
+            table {
+                
+                border-collapse: separate;
+                border-right: 1px solid #000000;
+                border-bottom: 1px solid #000000;
+                border-top: 1px solid #000000;
+                border-left: 1px solid #000000;
+
+
+            }
+        </style>
     </head>
     <body>
-        <h2>Select Options</h2>
-        
-        
+        <?php echo $displayBlock_adminPage; 
+        echo $displayTable;?> 
+
     </body>
 </html>
-
